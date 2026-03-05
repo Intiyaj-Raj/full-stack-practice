d047117c43579d96d3001ffc875994502d1ac1fa
Intiyaj-Raj <intiyajansari8757@gmail.com>
2026-03-06 02:16:34 +0530

    Enable button click

---
+++ CSS/mc-laren/script.js
@@ -0,0 +1,75 @@
+function home() {
+    document.querySelector(".home").style.display = "block";
+    document.querySelector(".racing-video").style.display = "none";
+    document.querySelector(".automotive").style.display = "none";
+    document.querySelector(".corporate").style.display = "none";
+    document.querySelector(".career").style.display = "none";
+    document.querySelector(".golf").style.display = "none"
+    document.querySelector(".store").style.display = "none"
+
+}
+
+function showRacingVid() {
+    document.querySelector(".home-video").style.display = "none";
+    document.querySelector(".automotive").style.display = "none";
+    document.querySelector(".corporate").style.display = "none";
+    document.querySelector(".career").style.display = "none";
+    document.querySelector(".golf").style.display = "none";
+    document.querySelector(".store").style.display = "none"
+    document.querySelector(".racing-video").style.display = "block";
+
+}
+
+function showAutomotiveVid() {
+    document.querySelector(".home-video").style.display = "none";
+    document.querySelector(".racing-video").style.display = "none";
+    document.querySelector(".corporate").style.display = "none";
+    document.querySelector(".career").style.display = "none";
+    document.querySelector(".golf").style.display = "none";
+    document.querySelector(".store").style.display = "none"
+    document.querySelector(".automotive").style.display = "block";
+}
+
+function showGolfVid() {
+    document.querySelector(".home-video").style.display = "none";
+    document.querySelector(".racing-video").style.display = "none";
+    document.querySelector(".corporate").style.display = "none";
+    document.querySelector(".career").style.display = "none";
+    document.querySelector(".automotive").style.display = "none";
+    document.querySelector(".store").style.display = "none"
+    document.querySelector(".golf").style.display = "block"
+
+}
+
+function showCorporateVid() {
+    document.querySelector(".home-video").style.display = "none";
+    document.querySelector(".racing-video").style.display = "none";
+    document.querySelector(".career").style.display = "none";
+    document.querySelector(".automotive").style.display = "none";
+    document.querySelector(".golf").style.display = "none"
+    document.querySelector(".store").style.display = "none"
+    document.querySelector(".corporate").style.display = "block"
+
+}
+
+function showCarrierVid() {
+    document.querySelector(".home-video").style.display = "none";
+    document.querySelector(".racing-video").style.display = "none";
+    document.querySelector(".automotive").style.display = "none";
+    document.querySelector(".golf").style.display = "none";
+    document.querySelector(".corporate").style.display = "none";
+    document.querySelector(".store").style.display = "none"
+    document.querySelector(".career").style.display = "block";
+
+}
+
+function showStoreVid() {
+    document.querySelector(".home-video").style.display = "none";
+    document.querySelector(".racing-video").style.display = "none";
+    document.querySelector(".automotive").style.display = "none";
+    document.querySelector(".golf").style.display = "none";
+    document.querySelector(".corporate").style.display = "none";
+    document.querySelector(".career").style.display = "none";
+    document.querySelector(".store").style.display = "block";
+
+}
