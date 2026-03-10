18afd9e50693fc69c23f20d7dfe8b13e1171a809
Intiyaj-Raj <intiyajansari8757@gmail.com>
2026-03-10 09:34:34 +0530

    add login page or love page

--- JavaScript/login-page/script.js
+++ JavaScript/login-page/script.js
@@ -2,36 +2,47 @@ function loginPage() {
 
     let email = document.getElementById("email").value;
     let pass = document.getElementById("pass").value;
-    let phone = document.getElementById("phone").value;
     let question = document.getElementById("question");
     let success = document.querySelector(".succes");
-    let form = document.getElementById("lform")
-    let ans = document.getElementById('ans').value;
-    let denied = document.querySelector(".denied")
-    let answer2 = document.getElementById("enter-answer")
-    let right = document.getElementById("right")
+    let form = document.getElementById("lform");
+    let ans = document.getElementById("ans").value;
+    let denied = document.querySelector(".denied");
+    let answer2 = document.getElementById("enter-answer");
+    let right = document.getElementById("right");
+    let link = document.getElementById("link");
 
-    if (email !== "" && pass !== "" && phone !== "") {
+    if (email !== "" && pass !== "") {
+
         question.style.display = "block";
 
         if (ans === "") {
             denied.style.display = "none";
             answer2.style.display = "block";
         }
-        else if (ans == "0") {
+
+        else if (ans === "0" || ans.toLowerCase() === "i miss you") {
+
             success.style.display = "block";
             form.style.display = "none";
             right.style.display = "none";
+            answer2.style.display = "none";
+            link.style.display = "block";
+
         }
+
         else {
             denied.style.display = "none";
             right.style.display = "block";
             answer2.style.display = "none";
         }
+
     }
+
     else {
+
         question.style.display = "none";
         denied.style.display = "block";
+
     }
 
 }
