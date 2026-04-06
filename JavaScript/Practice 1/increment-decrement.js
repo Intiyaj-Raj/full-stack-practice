8906701800f44bccdf0de402618c56bed4ee032d
Intiyaj-Raj <intiyajansari8757@gmail.com>
2026-04-06 20:54:07 +0530

    add quiz task

---
+++ JavaScript/Practice 1/increment-decrement.js
@@ -0,0 +1,15 @@
+let b = 7
+let a = 5
+let c = 6
+
+let d = a++ - --a - a
+console.log(d)
+
+
+let e = b++ + c-- - --c + c++ - b-- + d + --a - a--
+console.log(e)
+
+let t = a++ + ++a + ++a - b - b - c-- - d
+console.log(t)
+let r = a-- + --b - --c + 5 + 8 + t - e - d
+console.log(r)
