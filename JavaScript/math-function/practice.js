64f41a868e3ee45eb7de88001b880e9350c0080e
Intiyaj-Raj <intiyajansari8757@gmail.com>
2026-04-08 16:48:32 +0530

    practice math methods

---
+++ JavaScript/math-function/practice.js
@@ -0,0 +1,37 @@
+console.log("round")
+let a = 4.4;
+let b = 45.9
+console.log("a = " + a)
+console.log("b = " + b)
+console.log()
+// Number ko nearest integer me convert karta hai.
+console.log(Math.round(a))
+console.log(Math.round(b))
+
+console.log()
+
+// Hamesha neeche (lower integer) round karta hai.
+console.log("floor")
+console.log(Math.floor(a))
+console.log(Math.floor(b))
+
+console.log()
+
+// Hamesha upar (next integer) round karta hai.
+console.log("ceil")
+console.log(Math.ceil(a))
+console.log(Math.ceil(b))
+
+
+console.log()
+
+// Decimal part hata deta hai (sirf integer part rakhta hai).
+console.log("trunc method")
+console.log(Math.trunc(a))
+console.log(Math.trunc(b))
+
+
+console.log(Math.E)
+console.log(Math.LN10)
+console.log(Math.SQRT1_2)
+console.log(Math.SQRT2)
