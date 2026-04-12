e0edc4a8773005058d6924e83e1f437dd12d5cb3
Intiyaj-Raj <intiyajansari8757@gmail.com>
2026-04-12 17:56:41 +0530

    add task 7 form validation

---
+++ JavaScript/practice-task/task-7/script.js
@@ -0,0 +1,20 @@
+function formValidation() {
+
+    let userName = document.getElementById("uEmail").value;
+    let userPass = document.getElementById("uPass").value;
+
+    if (userName === "") {
+        alert("Enter username");
+        alert("Use intiyaj username to log in.")
+    }
+    else if (userPass === "") {
+        alert("Enter password");
+        alert("Use inti9117 username to log in.")
+    }
+    else if (userName === "intiyaj" && userPass === "inti9117") {
+        alert("Login Successful 😍");
+    }
+    else {
+        alert("Invalid username or password");
+    }
+}
