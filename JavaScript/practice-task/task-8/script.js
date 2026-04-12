251ec20b70b3c64c4763befd0b748ed651fa2faf
Intiyaj-Raj <intiyajansari8757@gmail.com>
2026-04-13 02:17:27 +0530

    fix error

--- JavaScript/practice-task/task-8/script.js
+++ JavaScript/practice-task/task-8/script.js
@@ -2,12 +2,20 @@ function handlePass() {
     let pass = document.getElementById("pass").value
     let passAlert = document.getElementById("passAlert")
     let checkIcon = document.getElementById("checkIcon")
+    let spinner = document.getElementById("spinner")
     if (pass.length < 10) {
-        passAlert.style.display = "block"
+        passAlert.style.display = "none"
         checkIcon.style.display = "none"
+        spinner.style.display = "block"
     }
     else if (pass.length >= 10) {
         passAlert.style.display = "none"
         checkIcon.style.display = "block"
+        spinner.style.display = "none"
     }
+}
+
+function handleInput() {
+    let passAlert = document.getElementById("passAlert")
+    passAlert.style.display = "block"
 }
