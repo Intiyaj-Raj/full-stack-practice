da5f6e99e735410b8a0cffbed10ce290643682f3
Intiyaj-Raj <intiyajansari8757@gmail.com>
2026-04-21 10:03:16 +0530

    basic of forEach loop

---
+++ JavaScript/Practice 1/loops/forEach.js
@@ -0,0 +1,27 @@
+let arr = [10, 20, 30, 40, 50, 60]
+
+arr.forEach((a) => {
+    console.log(a)
+})
+
+console.log()
+// print value + index number
+arr.forEach((a, b) => {
+    console.log("index = " + b)
+    console.log("value = " + a)
+});
+
+console.log()
+
+arr.forEach((value, index, array) => {
+    console.log("index:  " + index)
+    console.log("value: " + value)
+    console.log("array: " + array)
+    console.log()
+})
+
+
+arr.forEach(a => {
+    console.log(a)
+
+})
