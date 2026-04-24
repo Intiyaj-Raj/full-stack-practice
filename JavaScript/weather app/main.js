51a22ef11dd79f7739dc8459f097fbdc6b62ef38
Intiyaj-Raj <intiyajansari8757@gmail.com>
2026-04-24 21:37:01 +0530

    create weather app

---
+++ JavaScript/weather app/main.js
@@ -0,0 +1,44 @@
+let input = document.querySelector('input')
+let form = document.querySelector('form')
+let ans = document.getElementsByClassName('ans')[0]
+let img = document.getElementById('img')
+let cityName = document.getElementById('cityName')
+let tempC = document.getElementById('tempC')
+let tempF = document.getElementById('tempF')
+let state = document.getElementById('state')
+let country = document.getElementById('country')
+let date = document.getElementById('date')
+let time = document.getElementById('time')
+let timeZone = document.getElementById('timeZone')
+let lastUpdateTimeDate = document.getElementById('lastUpdateTimeDate')
+
+
+form.addEventListener('submit', weatherAPI)
+async function weatherAPI(e) {
+    try {
+        ans.style.opacity = "1"
+        e.preventDefault()
+        var key = "1868eecae1ae49ecb7262531232802"
+        var res = await fetch(`https://api.weatherapi.com/v1/current.json?key=${key}&q=${input.value}&aqi=yes`)
+        var data = await res.json()
+
+        //ans indore
+        img.setAttribute('src', `${data.current.condition.icon}`)
+        console.log(data);
+
+        cityName.innerText = data.location.name + " , "
+        state.innerText = data.location.region + " , "
+        country.innerText = data.location.country
+
+        tempC.innerText = data.current.temp_c
+        tempF.innerText = data.current.temp_f
+
+        time.innerText = "Time - " + data.location.localtime.split(" ")[1]
+        date.innerText = "Date - " + data.location.localtime.split(" ")[0]
+        timeZone.innerText = "Time zone - " + data.location.tz_id
+        lastUpdateTimeDate.innerText = "Last updated - " + data.current.last_updated
+        form.reset()
+    } catch (error) {
+        alert("Please Enter valid City name")
+    }
+}
