ca8a365bb1ad8facf0a97a09fc8be49469949706
Intiyaj-Raj <intiyajansari8757@gmail.com>
2026-05-09 20:09:13 +0530

    add auto count functionality

--- React/counter-app/src/assets/components/Counter.jsx
+++ React/counter-app/src/assets/components/Counter.jsx
@@ -1,20 +1,65 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Counter = (props) => {
+
+    const [autoCount, setAutoCount] = useState(false)
+
+    useEffect(() => {
+
+        let timer;
+
+        if (autoCount) {
+            timer = setInterval(() => {
+                props.increase()
+            }, 1000)
+        }
+
+        return () => clearInterval(timer)
+
+    }, [autoCount, props.count])
+
     return (
         <>
             <h1>Counter</h1>
+
             <div>
-                <h1 className='countVal' style={{ boxShadow: `${props.mode === "#000000e6" ? "#ffffffe6" : "#000000e6"}` }}>count = {props.count}</h1>
+                <h2
+                    className='countVal'
+                    style={{
+                        boxShadow:
+                            props.mode === "black"
+                                ? "0 0 10px 2px #ccc"
+                                : "0 0 10px 2px rgba(0,0,0,0.5)"
+                        ,
 
-                <span>
-                    <button onClick={props.increase}>+</button>
-                    <button onClick={props.decrease}>-</button>
-                    <button onClick={props.reset}>reset</button>
-                    <button onClick={props.setDarkMode}>Dark</button>
+                    }}
+                >
+                    Count = <span className='counterValStore'>{props.count}</span>
+                </h2>
+
+                <span className='counter_btn'>
+                    <button onClick={props.increase}>Increase (+)</button>
+
+                    <button onClick={props.decrease}>Decrease (-)</button>
+
+                    <button onClick={props.reset}>Reset</button>
+
+                    <button onClick={props.setDarkMode}>
+                        {props.mode === "white"
+                            ? "Dark Mode"
+                            : "Light Mode"}
+                    </button>
+
+                    <button
+                        onClick={() => setAutoCount(!autoCount)}
+                        style={{ marginTop: "10px" }}
+                    >
+                        {autoCount
+                            ? "Stop Auto Count"
+                            : "Start Auto Count"}
+                    </button>
                 </span>
-
-            </div>
+            </div >
         </>
     )
 }
