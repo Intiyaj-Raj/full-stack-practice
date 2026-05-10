0fec11efac38bfbcbafb4226faecc8d3fe9583e4
Intiyaj-Raj <intiyajansari8757@gmail.com>
2026-05-10 06:07:47 +0530

    fix file structure

--- React/counter-app/src/App.jsx
+++ React/counter-app/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import './App.css'
-import Counter from './assets/components/Counter'
+import Counter from './components/Counter'
 const App = () => {
 
   const [count, setCount] = useState(0)
