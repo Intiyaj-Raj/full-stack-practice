b233f8185b0319918739f440f7b89b74036e2fa5
Intiyaj-Raj <intiyajansari8757@gmail.com>
2026-05-21 17:24:56 +0530

    add more secton ui

--- JavaScript/Sundown Studio/script.js
+++ JavaScript/Sundown Studio/script.js
@@ -2,3 +2,7 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+let a = document.querySelector("#elem1")
+let abc = a.getAttribute("data-image")
+console.log(abc)
