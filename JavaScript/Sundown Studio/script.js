c19a6ba0e689a9565a40fa6c47a8ceaf0882cdf0
Intiyaj-Raj <intiyajansari8757@gmail.com>
2026-05-22 12:10:38 +0530

    improve its design

--- JavaScript/Sundown Studio/script.js
+++ JavaScript/Sundown Studio/script.js
@@ -5,4 +5,37 @@ const scroll = new LocomotiveScroll({
 
 let a = document.querySelector("#elem1")
 let abc = a.getAttribute("data-image")
-console.log(abc)
+// console.log(abc)
+
+function page4Animation() {
+    var fixed = document.querySelector("#fixed-image")
+    let elemC = document.querySelector("#elem-container")
+
+    elemC.addEventListener("mouseenter", function () {
+        // alert("heyyyyyy")
+        fixed.style.display = "block"
+    })
+    elemC.addEventListener("mouseleave", function () {
+        // alert("heyyyyyy")
+        fixed.style.display = "none"
+    })
+
+    var elems = document.querySelectorAll(".elem")
+    elems.forEach(function (e) {
+        e.addEventListener("mouseenter", function () {
+            var image = e.getAttribute('data-image')
+            fixed.style.backgroundImage = `url(${image})`
+
+        })
+    })
+}
+
+function swiperAnimation() {
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: "auto",
+        centeredSlides: true,
+        spaceBetween: 100,
+    });
+}
+swiperAnimation()
+page4Animation()
