5efd3c35e221274e52b0f6e5380f84ffe55ae364
Intiyaj-Raj <intiyajansari8757@gmail.com>
2026-06-14 03:58:47 +0530

    practice on ejs

--- Backend/ejs/index.js
+++ Backend/ejs/index.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require("path");
 const port = 3000;
 
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
@@ -13,13 +14,35 @@ app.get("/", (req, res) => {
 
 // rolldice  ejs file
 app.get("/roll", (req, res) => {
-  res.render("rolldice.ejs");
+  let diceVal = Math.floor(Math.random() * 6) + 1;
+  res.render("rolldice.ejs", { diceVal });
 });
 
 // instagram.ejs file
 app.get("/ig/:username", (req, res) => {
   let { username } = req.params;
   res.render("instagram.ejs", { username });
+});
+
+// linkedin.ejs
+app.get("/linkedin/:username", (req, res) => {
+  let follwers = ["john", "alia", "bob", "marsh", "kane", "michel"];
+  let { username } = req.params;
+  res.render("linkedin.ejs", { username, follwers });
+});
+
+// instagram page with json data
+
+app.get("/instagram/:username", (req, res) => {
+  let { username } = req.params;
+  const data = require("./instaData.json");
+  let instaData = data[username];
+  if (instaData) {
+    console.log(instaData);
+    res.render("instagramData.ejs", { instaData });
+  } else {
+    res.render("error.ejs");
+  }
 });
 
 app.listen(port, () => {
