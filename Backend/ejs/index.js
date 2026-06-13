29f849fb98afb584d1ba3081078a674ab752606b
Intiyaj-Raj <intiyajansari8757@gmail.com>
2026-06-14 02:46:20 +0530

    practice on ejs

--- Backend/ejs/index.js
+++ Backend/ejs/index.js
@@ -11,7 +11,16 @@ app.get("/", (req, res) => {
   res.render("home.ejs");
 });
 
-// instagram ejs file
+// rolldice  ejs file
+app.get("/roll", (req, res) => {
+  res.render("rolldice.ejs");
+});
+
+// instagram.ejs file
+app.get("/ig/:username", (req, res) => {
+  let { username } = req.params;
+  res.render("instagram.ejs", { username });
+});
 
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
