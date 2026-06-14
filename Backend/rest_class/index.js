4ac547c8d10e0c100ab65dbb86977c562e914b6e
Intiyaj-Raj <intiyajansari8757@gmail.com>
2026-06-15 00:20:49 +0530

    add edit logic

--- Backend/rest_class/index.js
+++ Backend/rest_class/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const { v4: uuidv4 } = require("uuid");
-
 app.use(express.urlencoded({ extended: true }));
+const methodOverride = require("method-override");
+
+app.use(methodOverride("_method"));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -55,6 +57,22 @@ app.get("/posts/:id", (req, res) => {
   let post = posts.find((p) => p.id === id);
   res.render("show.ejs", { post });
 });
+
+// edit post
+app.patch("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let newContent = req.body.content;
+  let post = posts.find((p) => p.id === id);
+  post.content = newContent;
+  res.redirect("/posts");
+});
+
+app.get("/posts/:id/edit", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p) => p.id === id);
+  res.render("edit.ejs", { post });
+});
+
 app.listen(3000, (req, res) => {
   console.log("linstening to port : 3000");
 });
